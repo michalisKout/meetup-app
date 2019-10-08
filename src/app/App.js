5f5684b93d92c6c833cbeaf16c249e1faf2a0acc
Michalis Koutridis <michalis.koutridis@gmail.com>
2019-10-09 00:52:18 +0300

    Fixes router refresh page not found error on a static site generator.

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import EventSectionContainer from './components/container/EventSectionContainer';
 import MyEvents from './components/presentational/events/MyEvents';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
@@ -7,7 +8,7 @@ import Header from './components/presentational/header/Header';
 
 const App = () => {
   return (
-    <Router>
+    <Router history={createBrowserHistory()}>
       <ErrorBoundary>
         <Header />
         <main>
