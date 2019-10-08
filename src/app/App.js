b42c16245d92828d2e38bdba9c3a387a710c1228
Michalis Koutridis <michalis.koutridis@gmail.com>
2019-10-09 01:04:31 +0300

    Replaces createBrowserHistory with createHashHistory.

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import EventSectionContainer from './components/container/EventSectionContainer';
 import MyEvents from './components/presentational/events/MyEvents';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import Header from './components/presentational/header/Header';
+import NotFound from './components/presentational/NotFound';
 
 const App = () => {
   return (
-    <Router history={createBrowserHistory()}>
+    <Router history={createHashHistory()}>
       <ErrorBoundary>
         <Header />
         <main>
           <Switch>
             <Route path={['/', '/events']} exact component={EventSectionContainer} />
             <Route path="/myevents" component={MyEvents} />
+            <Route component={NotFound} />
           </Switch>
         </main>
       </ErrorBoundary>
